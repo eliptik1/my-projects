a4c6105fe62d050b796beaba0cfe6274b5a37f33
Eliptik <adamfeyk5@gmail.com>
2024-01-25 16:39:52 +0300

    Add data to document by clicking the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import db from "./firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  doc,
+  addDoc,
+  setDoc,
+} from "firebase/firestore";
 
 const Dot = ({ color }) => {
   const style = {
@@ -18,6 +24,19 @@ const Dot = ({ color }) => {
 function App() {
   const [colors, setColors] = useState([{ name: "Loading...", id: "initial" }]);
 
+  const handleNew = async () => {
+    console.log("clicked");
+    const name = prompt("enter color name");
+    const value = prompt("enter color value");
+    // const docRef = doc(db, "colors", "color001");
+    // const payload = { name: "Black", value: "#000" };
+    // await setDoc(docRef, payload);
+
+    const collectionRef = collection(db, "colors");
+    const payload = { name: name, value: value };
+    const docRef = await addDoc(collectionRef, payload);
+    console.log("The new ID is: ", docRef.id);
+  };
   useEffect(() => {
     onSnapshot(collection(db, "colors"), (snapshot) => {
       const myData = [];
@@ -31,7 +50,7 @@ function App() {
   console.log(colors);
   return (
     <div>
-      <button>New</button>
+      <button onClick={handleNew}>New</button>
       <ul>
         {colors.map((color) => (
           <li key={color.id}>
