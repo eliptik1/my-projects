25f1428f9bebd58f519386d9e6bc6184118905e7
Eliptik <aliyurdunol@gmail.com>
2024-04-24 18:23:13 +0300

    Create a react project for learning useContext hook

---
+++ react-test/vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Heading from './Heading.jsx';
+import Section from './Section.jsx';
+
+export default function App() {
+  return (
+    <Section >
+      <Heading>Title</Heading>
+      <Section >
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section >
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section >
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
